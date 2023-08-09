cabf8f2cb73bc5c9cc566fdebd55137aa782f460
eriKalucero <135881380+eriKalucero@users.noreply.github.com>
2023-08-09 18:27:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel= "stylesheet" type="text/css" href="./css/css-login3.css"/> 
+</head>
+    <body> 
+        <header>
+            <div class=logo>
+            <a href= trabajo.php>
+            <img src="img/user.jpeg" class="logo" >
+             <h2>Bienvenido</h2>   
+            </div>
+        </header>
+    <?php
+    include("imagenbd.php")
+    ?>
+    <footer class="footer">
+        <p> footer </p>
+    </footer>
+</body>
+</html>
+
---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel= "stylesheet" type="text/css" href="./css/css-pagprincipal.css"/> 
+</head>
+<body> 
+    <header>
+        <div class="cabecera">
+            <img class="logo" src="./img/user.png" alt="logo"> 
+            <h1>okkkkkkk</h1>
+        </div>
+    </header>
+    <a href="pgprincipal.html" target="_blank">
+            <button class="boton">ALUMNO</button>
+            <button class="boton">ALUMNO</button>
+    </a>
+    <footer class="footer">
+        <p>ahre</p>
+    </footer>
+</body>
+</html>
